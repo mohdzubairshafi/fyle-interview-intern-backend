e9bdd4ef4e230bbf3ee6a0eb58c85b8a4844bfe7
Mohd Zubair Ansari <114806277+mohdzubairshafi@users.noreply.github.com>
2023-05-03 02:00:15 +0000

    initial work  added

--- core/apis/responses.py
+++ core/apis/responses.py
@@ -5,3 +5,9 @@ class APIResponse(Response):
     @classmethod
     def respond(cls, data):
         return make_response(jsonify(data=data))
+    @classmethod
+    def respond_error(cls, message, status_code):
+        response = {
+            "error": message
+        }
+        return make_response(jsonify(response), status_code)
